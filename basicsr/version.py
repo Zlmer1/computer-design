ebcfdd6ce43c6d282fd859dbbe5fbc9691f52111
李铭炜 <141426247+Zlmer1@users.noreply.github.com>
2025-04-15 21:06:37 +0800

    setup

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Apr 14 20:43:14 2025
-__version__ = '1.2.0+10018c6'
+# TIME: Tue Apr 15 12:32:40 2025
+__version__ = '1.2.0+1b27046'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
